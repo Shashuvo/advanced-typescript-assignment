ed9f96cd26493c41d42cc322336224a77ae36e2e
Shashuvo <shahariat2810@gmail.com>
2026-05-07 15:28:50 +0600

    problem 3 solved

--- solutions.ts
+++ solutions.ts
@@ -19,4 +19,21 @@ const reverseString = (value: string) => {
 
 reverseString("typescript");
 
+// <-- problem 3 -->
+type StringOrNumber = string | number;
+
+const checkType = (value: StringOrNumber)=>{
+    if (typeof value === "number") {
+        console.log(`"Number"`);
+    }
+    else if (typeof value === "string") {
+        console.log(`"String"`);
+    }
+}
+
+checkType("Hello");
+checkType(42);
+
+
+// <-- problem 4 -->
 
